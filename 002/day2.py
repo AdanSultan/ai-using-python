899ecb62ff8aaab7c1f318134ee749ce0735fd84
AdanSultan <adansultan794@gmail.com>
2026-05-31 21:05:52 +0500

    Fimiliar with how to change data type of a variable

---
+++ 002/day2.py
@@ -0,0 +1,12 @@
+# Type Casting & Input Functions 
+# Means changing the type of a variable & input enter by the user 
+
+age = "34"
+print(type(age))
+age = int(age)
+age = age + 1
+print(age)
+# age = age+1
+print(type(age))
+
+
