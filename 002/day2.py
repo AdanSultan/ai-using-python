c2c1351528ae3afa7393f7fc6cef5bad08bfa14b
AdanSultan <adansultan794@gmail.com>
2026-07-16 23:12:43 +0500

    Understand the input function & how we can convert its data type

--- 002/day2.py
+++ 002/day2.py
@@ -1,12 +1,23 @@
 # Type Casting & Input Functions 
 # Means changing the type of a variable & input enter by the user 
 
-age = "34"
-print(type(age))
-age = int(age)
-age = age + 1
-print(age)
-# age = age+1
-print(type(age))
+# age = "34"
+# print(type(age))
+# age = int(age)
+# age = age + 1
+# print(age)
+# # age = age+1
+# print(type(age))
 
 
+# Input Function how can input enter by the user 
+
+# age = input("Please write your age : ")
+
+# Input always enter in the string format 
+
+# myage = int(age) # You can change the type of the string 
+
+
+age1 = int(input("Enter your age : "))
+print(type(age1))
