94bd235f307075b94baf1fbf1f8c5560ec2a9b68
AdanSultan <adansultan794@gmail.com>
2026-07-28 18:43:55 +0500

    Learning about libraries & from where we can import and use datasets

--- 004/steps_for_data_analysis.py
+++ 004/steps_for_data_analysis.py
@@ -1,1 +1,19 @@
 # Today we learn 9 different steps which we follow as a Data Analyst or Data Scientist
+
+# 1st Data Collection 
+
+# Ways: Manual Data Entry , API or Web Scrapping , Data Bases 
+# Two types of data 
+# Primary : Collect by their own 
+# Secondary Data : Other's Data 
+
+# Web-Sites for Data 
+# Google data search , kaggle , Github , UCI machine learning repository 
+# Few libraries for built in dataset , seaborn , socket-learn , yahu finance stock market data 
+
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+sns.load_dataset('titanic')
